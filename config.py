fed62f5ff9065f7344c1ed3790a878063a17920b
pooranji72 <pooran.ji.72@gmail.com>
2024-10-11 00:58:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7443034654:AAH83ai22x0R_um_D4zHToFIiCAv8M1q4Co"
 OWNER_ID = list(map(int, getenv("OWNER_ID", "5315677437").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://amit:amitrajera@now.xhnas.mongodb.net/?retryWrites=true&w=majority&appName=now")
 LOG_GROUP = getenv("LOG_GROUP", "yuy1326")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "1002314107303"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002314107303"))
